559f213844df6579ac348348a33382c51be771a1
KulondwaLandry <landrymufundi@gmail.com>
2024-01-08 06:07:24 -0300

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/kulondwa/portofolio.v1"
                 target="_blank"
                 className="fork-btn-inner"
               >
